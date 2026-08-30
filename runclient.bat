80d3eeefdac1a468708a3e3f9f9d8bc17b5385d0
10MeV <863649106@qq.com>
2026-08-30 12:01:12 +0800

    完善一些机器，增加代码参考的说明和鸣谢

--- runclient.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-
-REM Compatibility entry point: keep the conventional "runclient" name while
-REM delegating to the maintained client launcher.
-call "%~dp0run-client.bat"
-exit /b %ERRORLEVEL%
